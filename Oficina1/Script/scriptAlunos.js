d090af2041dd3bbaea7b464984cda0dcf28b3a60
João Pedro Odorizzio de Souza <joao.310502@alunos.utfpr.edu.br>
2023-12-07 11:54:06 -0300

    Refactor HTML and CSS code for better structure and readability

--- Oficina1/Script/scriptAlunos.js
+++ Oficina1/Script/scriptAlunos.js
@@ -1,32 +1,114 @@
-function preencherTabela() {
-    // Suponha que você tenha dados de alunos em um array
-    var dadosAlunos = [
-        { nome: "João da Silva", escola: "Escola Municipal de Ensino Fundamental Professora Maria de Lourdes Teixeira", turma: "1º Ano", id: "123456789" },
-        // Adicione mais dados conforme necessário
-    ];
+function populateTable(dados) {
+    if (dados && dados.length > 0) {
+        const tableBody = document.querySelector('#tabelaAlunos tbody');
+        tableBody.innerHTML = '';
 
-    var tabela = document.getElementById("tabelaAlunos").getElementsByTagName('tbody')[0];
+        dados.forEach(aluno => {
+            const newRow = tableBody.insertRow();
 
-    // Limpa o conteúdo atual da tabela
-    tabela.innerHTML = "";
+            const nomeCell = newRow.insertCell(0);
+            const nomeLink = document.createElement('a');
+            nomeLink.href = '../Notas/notas.html'; 
+            nomeLink.textContent = aluno.nome;
+            nomeCell.appendChild(nomeLink);
 
-    for (var i = 0; i < dadosAlunos.length; i++) {
-        var aluno = dadosAlunos[i];
+            const escolaCell = newRow.insertCell(1);
+            escolaCell.textContent = aluno.escola;
 
-        var novaLinha = tabela.insertRow(tabela.rows.length);
-        var colunaNome = novaLinha.insertCell(0);
-        var colunaEscola = novaLinha.insertCell(1);
-        var colunaTurma = novaLinha.insertCell(2);
-        var colunaID = novaLinha.insertCell(3);
-        var colunaEditar = novaLinha.insertCell(4);
+            const turmaCell = newRow.insertCell(2);
+            turmaCell.textContent = aluno.turma;
 
-        colunaNome.innerHTML = aluno.nome;
-        colunaEscola.innerHTML = aluno.escola;
-        colunaTurma.innerHTML = aluno.turma;
-        colunaID.innerHTML = aluno.id;
-        colunaEditar.innerHTML = '<img src="../imagens/Editor2.png" alt="icone Editor2">';
+            const idCell = newRow.insertCell(3);
+            idCell.textContent = aluno.id;
+        });
     }
 }
 
-// Chamando a função para preencher a tabela ao carregar a página
-window.onload = preencherTabela;
+const dados = [
+    {
+        nome: 'Aluno 1',
+        escola: 'Escola 1',
+        turma: 'Turma A',
+        id: '1'
+    },
+    {
+        nome: 'Aluno 2',
+        escola: 'Escola 2',
+        turma: 'Turma B',
+        id: '2'
+    },
+    {
+        nome: 'Aluno 3',
+        escola: 'Escola 3',
+        turma: 'Turma C',
+        id: '3'
+    },
+    {
+        nome: 'Aluno 4',
+        escola: 'Escola 4',
+        turma: 'Turma D',
+        id: '4'
+    },
+    {
+        nome: 'Aluno 5',
+        escola: 'Escola 5',
+        turma: 'Turma E',
+        id: '5'
+    },
+    {
+        nome: 'Aluno 6',
+        escola: 'Escola 6',
+        turma: 'Turma F',
+        id: '6'
+    },
+    {
+        nome: 'Aluno 7',
+        escola: 'Escola 7',
+        turma: 'Turma G',
+        id: '7'
+    },
+    {
+        nome: 'Aluno 8',
+        escola: 'Escola 8',
+        turma: 'Turma H',
+        id: '8'
+    },
+    {
+        nome: 'Aluno 9',
+        escola: 'Escola 9',
+        turma: 'Turma I',
+        id: '9'
+    },
+    {
+        nome: 'Aluno 10',
+        escola: 'Escola 10',
+        turma: 'Turma J',
+        id: '10'
+    },
+    {
+        nome: 'Aluno 11',
+        escola: 'Escola 11',
+        turma: 'Turma K',
+        id: '11'
+    },
+    {
+        nome: 'Aluno 12',
+        escola: 'Escola 12',
+        turma: 'Turma L',
+        id: '12'
+    },
+    {
+        nome: 'Aluno 13',
+        escola: 'Escola 13',
+        turma: 'Turma M',
+        id: '13'
+    },
+    {
+        nome: 'Aluno 14',
+        escola: 'Escola 14',
+        turma: 'Turma N',
+        id: '14'
+    }
+];
+
+populateTable(dados);
--- Oficina1/Script/scriptEscolas.js
+++ Oficina1/Script/scriptEscolas.js
@@ -1,155 +1,186 @@
+const dados = [
+    {
+        nome: 'Escola 1',
+        endereco: 'Rua 1',
+        contato: '1111-1111'
+    },
+    {
+        nome: 'Escola 2',
+        endereco: 'Rua 2',
+        contato: '2222-2222'
+    },
+    {
+        nome: 'Escola 3',
+        endereco: 'Rua 3',
+        contato: '3333-3333'
+    },
+    {
+        nome: 'Escola 4',
+        endereco: 'Rua 4',
+        contato: '4444-4444'
+    },
+    {
+        nome: 'Escola 5',
+        endereco: 'Rua 5',
+        contato: '5555-5555'
+    },
+    {
+        nome: 'Escola 6',
+        endereco: 'Rua 6',
+        contato: '6666-6666'
+    },
+    {
+        nome: 'Escola 7',
+        endereco: 'Rua 7',
+        contato: '7777-7777'
+    },
+    {
+        nome: 'Escola 8',
+        endereco: 'Rua 8',
+        contato: '8888-8888'
+    },
+    {
+        nome: 'Escola 9',
+        endereco: 'Rua 9',
+        contato: '9999-9999'
+    },
+    {
+        nome: 'Escola 10',
+        endereco: 'Rua 10',
+        contato: '1010-1010'
+    },
+    {
+        nome: 'Escola 11',
+        endereco: 'Rua 11',
+        contato: '1111-1111'
+    },
+    {
+        nome: 'Escola 12',
+        endereco: 'Rua 12',
+        contato: '1212-1212'
+    },
+    {
+        nome: 'Escola 13',
+        endereco: 'Rua 13',
+        contato: '1313-1313'
+    },
+    {
+        nome: 'Escola 14',
+        endereco: 'Rua 14',
+        contato: '1414-1414'
+    },
+    {
+        nome: 'Escola 15',
+        endereco: 'Rua 15',
+        contato: '1515-1515'
+    },
+    {
+        nome: 'Escola 16',
+        endereco: 'Rua 16',
+        contato: '1616-1616'
+    },
+    {
+        nome: 'Escola 17',
+        endereco: 'Rua 17',
+        contato: '1717-1717'
+    },
+    {
+        nome: 'Escola 18',
+        endereco: 'Rua 18',
+        contato: '1818-1818'
+    },
+    {
+        nome: 'Escola 19',
+        endereco: 'Rua 19',
+        contato: '1919-1919'
+    },
+    {
+        nome: 'Escola 20',
+        endereco: 'Rua 20',
+        contato: '2020-2020'
+    },
+    {
+        nome: 'Escola 21',
+        endereco: 'Rua 21',
+        contato: '2121-2121'
+    },
+    {
+        nome: 'Escola 22',
+        endereco: 'Rua 22',
+        contato: '2222-2222'
+    },
+    {
+        nome: 'Escola 23',
+        endereco: 'Rua 23',
+        contato: '2323-2323'
+    },
+    
+
+    
+];
+
+async function populateTable(dados) {
+    if (dados && dados.length > 0) {
+        const tableBody = document.querySelector('#tabelaEscolas tbody');
+        tableBody.innerHTML = '';
+
+        dados.forEach(escola => {
+            const newRow = tableBody.insertRow();
+
+            const nomeCell = newRow.insertCell(0);
+            const nomeLink = document.createElement('a');
+            nomeLink.href = '../Turmas/turmas.html'; 
+            nomeLink.textContent = escola.nome;
+            nomeCell.appendChild(nomeLink);
+    
+
+            const enderecoCell = newRow.insertCell(1);
+            enderecoCell.textContent = escola.endereco;
+
+            const contatoCell = newRow.insertCell(2);
+            contatoCell.textContent = escola.contato;
+
+            const editCell = newRow.insertCell(3);
+            var imagem = document.createElement('img');
+            imagem.src = '../imagens/Editor2.png';
+            imagem.style.float = 'right';
+            editCell.appendChild(imagem);
+        });
+    }
+}
+populateTable(dados);
+
+function adicionarEscola() {
+    const nomeEscola = document.getElementById('nomeEscola').value;
+    const enderecoEscola = document.getElementById('enderecoEscola').value;
+    const contatoEscola = document.getElementById('contatoEscola').value;
+
+    const novaEscola = {
+        nome: nomeEscola,
+        endereco: enderecoEscola,
+        contato: contatoEscola
+    };
+
+    dados.push(novaEscola); 
+
+    populateTable(dados);
+
+    fecharModal(); 
+}
+
+function fecharModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
+
 function mostrarModal() {
     document.getElementById('nomeEscola').value = '';
     document.getElementById('enderecoEscola').value = '';
     document.getElementById('contatoEscola').value = '';
-    
-    document.getElementById('modal').style.display = 'block';
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'block';
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
 }
-
-function fecharModal() {
-    document.getElementById('modal').style.display = 'none';
-}
-
-function adicionarEscola() {
-    var nomeEscola = document.getElementById('nomeEscola').value;
-    var enderecoEscola = document.getElementById('enderecoEscola').value;
-    var contatoEscola = document.getElementById('contatoEscola').value;
-    
-    if (nomeEscola && enderecoEscola && contatoEscola) {
-        
-        var tabela = document.getElementById('tabelaEscolas').getElementsByTagName('tbody')[0];
-        var novaLinha = tabela.insertRow(tabela.rows.length);
-        var celulaNome = novaLinha.insertCell(0);
-        var celulaEndereco = novaLinha.insertCell(1);
-        var celulaContato = novaLinha.insertCell(2);
-
-        
-        celulaNome.innerHTML = nomeEscola;
-        celulaEndereco.innerHTML = enderecoEscola;
-        celulaContato.innerHTML = contatoEscola;
-
-        var imagem = document.createElement('img');
-            imagem.src = '../imagens/Editor2.png'; 
-            imagem.alt = 'Ícone do editor'; 
-            imagem.style.float = 'right'
-
-            celulaContato.appendChild(imagem);
-        
-        fecharModal();
-    } else {
-        alert('Por favor, preencha todos os campos.');
-    }
-}   
-
-document.addEventListener('DOMContentLoaded', function () {
-    var tabelaEscolas = document.getElementById('tabelaEscolas');
-    var tabelaTurmas = document.getElementById('tabelaTurmas');
-    var tabelaAlunos = document.getElementById('tabelaAlunos');
-    var tabelaNotas = document.getElementById('tabelaNotas');
-    var h2NomeEscola = document.getElementById('nomeEscola');
-
-    function mostrarTurmas(nomeEscola) {
-        var turmas = ['Turma A', 'Turma B', 'Turma C'];
-
-        tabelaTurmas.style.display = 'table';
-        tabelaTurmas.getElementsByTagName('tbody')[0].innerHTML = '';
-        tabelaAlunos.style.display = 'none';
-        tabelaNotas.style.display = 'none'; 
-
-        h2NomeEscola.innerText = nomeEscola;
-
-        for (var i = 0; i < turmas.length; i++) {
-            var novaLinha = tabelaTurmas.insertRow(tabelaTurmas.rows.length);
-            var celulaTurma = novaLinha.insertCell(0);
-            celulaTurma.innerHTML = turmas[i];
-        }
-        
-    }
-
-    function mostrarAlunosDaTurma(nomeTurma) {
-        var alunosPorTurma = {
-            'Turma A': [{ nome: 'Aluno 1', id: 1,}, { nome: 'Aluno 2', id: 2 }, {nome: 'Aluno 3', id: 3 }],
-            'Turma B': [{ id: 4, nome: 'Aluno 4' }, { id: 5, nome: 'Aluno 5' }, { id: 6, nome: 'Aluno 6' }],
-            'Turma C': [{ id: 7, nome: 'Aluno 7' }, { id: 8, nome: 'Aluno 8' }, { id: 9, nome: 'Aluno 9' }]
-        };
-    
-        tabelaAlunos.getElementsByTagName('tbody')[0].innerHTML = '';
-        tabelaNotas.style.display = 'none';
-        h2NomeEscola.innerText = nomeTurma;
-    
-        var alunos = alunosPorTurma[nomeTurma];
-    
-        for (var i = 0; i < alunos.length; i++) {
-            var novaLinha = tabelaAlunos.insertRow(tabelaAlunos.rows.length);
-            var celulaAluno = novaLinha.insertCell(0);
-            var celulaID = novaLinha.insertCell(1);
-    
-            celulaID.innerHTML = alunos[i].id;
-            celulaAluno.innerHTML = alunos[i].nome;
-        }
-    
-        tabelaAlunos.style.display = 'table';
-        tabelaTurmas.style.display = 'none';
-    }
-    
-    function mostrarNotasDoAluno(nomeAluno) {
-        var notasPorAluno = {
-            'Aluno 1': { materia: 'Matemática', nota1: 8, nota2: 7, nota3: 9, nota4: 8,},
-            'Aluno 2': { materia: 'Português', nota1: 7, nota2: 6, nota3: 8, nota4: 7 },
-            'Aluno 3': { materia: 'Ciências', nota1: 9, nota2: 8, nota3: 9, nota4: 10 }
-        };
-
-        var notas = notasPorAluno[nomeAluno];
-
-        tabelaNotas.getElementsByTagName('tbody')[0].innerHTML = '';
-        h2NomeEscola.innerText = 'Notas de ' + nomeAluno;
-
-        var novaLinha = tabelaNotas.insertRow(tabelaNotas.rows.length);
-        var celulaMateria = novaLinha.insertCell(0);
-        var celulaNota1 = novaLinha.insertCell(1);
-        var celulaNota2 = novaLinha.insertCell(2);
-        var celulaNota3 = novaLinha.insertCell(3);
-        var celulaNota4 = novaLinha.insertCell(4);
-
-        celulaMateria.innerHTML = notas.materia;
-        celulaNota1.innerHTML = notas.nota1;
-        celulaNota2.innerHTML = notas.nota2;
-        celulaNota3.innerHTML = notas.nota3;
-        celulaNota4.innerHTML = notas.nota4;
-
-        tabelaNotas.style.display = 'table';
-        tabelaAlunos.style.display = 'none';
-    }
-
-    tabelaEscolas.addEventListener('click', function (event) {
-        var target = event.target;
-
-        if (target.tagName === 'TD') {
-            var nomeEscola = target.parentElement.cells[0].innerText;
-
-            mostrarTurmas(nomeEscola);
-
-            tabelaEscolas.style.display = 'none';
-        }
-    });
-
-    tabelaTurmas.addEventListener('click', function (event) {
-        var target = event.target;
-
-        if (target.tagName === 'TD') {
-            var nomeTurma = target.innerText;
-
-            mostrarAlunosDaTurma(nomeTurma);
-        }
-    });
-
-    tabelaAlunos.addEventListener('click', function (event) {
-        var target = event.target;
-
-        if (target.tagName === 'TD') {
-            var nomeAluno = target.innerText;
-
-            mostrarNotasDoAluno(nomeAluno);
-        }
-    });
-});
