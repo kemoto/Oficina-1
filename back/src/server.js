842531565ec3d51c0e59269925cc20ded8c72f5e
Vitor Tominaga <vtominaga@hotmail.com>
2023-12-12 10:25:24 -0300

    .

--- back/src/server.js
+++ back/src/server.js
@@ -2,8 +2,10 @@ require("express-async-errors");
 const express = require("express");
 const AppError = require("./utils/AppError");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
